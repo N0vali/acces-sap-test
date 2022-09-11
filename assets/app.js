cbeb9318b3d7ab7618ed9b6fd34bec877ee990b3
n0vali <alicia.pilar83@gmail.com>
2022-09-11 19:51:03 +0200

    authentificator and user done

--- assets/app.js
+++ assets/app.js
@@ -4,9 +4,9 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
+// Import our custom CSS
+import './styles/app.scss'
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.scss';
-
-// start the Stimulus application
-import './bootstrap';
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+import "bootswatch/dist/morph/bootstrap.min.css";
